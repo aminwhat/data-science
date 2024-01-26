bc9822fb7658eefe3a4bf398279a6b1287872a48
aminwhat <m.aminwhat@gmail.com>
2024-01-26 14:25:24 +0330

    Add Builds and Comments to the Extra

--- 2.quick_sort/src/extra/app.py
+++ 2.quick_sort/src/extra/app.py
@@ -7,10 +7,18 @@ from game import random_number, make_array, identify
 
 class App:
     def __init__(self) -> None:
+        """
+        Constructor for the app
+        Will Request the user input from the cli arguments
+        """
         self.user_in = self.get_user_input()
         self.t1 = time.time()
 
     def get_user_input(self) -> int:
+        """
+        Get and validate the user input
+        """
+
         user_in: int = -1
         try:
             parser = argparse.ArgumentParser()
@@ -23,15 +31,28 @@ class App:
         return user_in
 
     def generate_random_number(self, user_in: int):
+        """
+        Generate the random number based on the user_input size
+        """
         return random_number(user_in)
 
     def make_array(self, arr):
+        """
+        Convert a simple array to a numpy array
+        """
         return make_array(arr)
 
     def get_max_val(self, array: np.ndarray) -> int:
+        """
+        Get the max value of a numpy array
+        """
         return array.max()
 
     def identify(self, maxVal: int):
+        """
+        Identify the generated values to Win or Fail the Game
+        Then Calculate the time duraing execution
+        """
         identify(maxVal)
         self.t2 = time.time()
         print("Time To Execute: " + str(self.t2 - self.t1))
--- 2.quick_sort/src/extra/game.py
+++ 2.quick_sort/src/extra/game.py
@@ -2,6 +2,10 @@ import numpy as np
 
 
 def identify(maxVal: int):
+    """
+    Determine where the max value is under condition to lose or win
+    """
+
     if maxVal > 70:
         print("Win")
     else:
@@ -9,10 +13,16 @@ def identify(maxVal: int):
 
 
 def make_array(arr):
+    """
+    Make a numpy array from a simple array
+    """
     narr = np.array(arr)
     return narr
 
 
 def random_number(user_input: int):
+    """
+    Generate a Random numpy array based on the size from the user_input
+    """
     rand_vals = np.random.randint(0, 101, size=user_input)
     return rand_vals
